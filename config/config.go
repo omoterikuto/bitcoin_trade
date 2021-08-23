1ddfbf5d0c2249b7e978f55d43edc1f65625a56a
omoterikuto <omoterikuto@gmail.com>
2021-08-23 23:01:32 +0900

    db周り

--- config/config.go
+++ config/config.go
@@ -3,16 +3,22 @@ package config
 import (
 	"log"
 	"os"
+	"time"
 
 	"gopkg.in/ini.v1"
 )
 
 //APIInfo bitflyer API
 type APIInfo struct {
-	APISecret   string
-	APIKey      string
-	LogFile     string
-	ProductCode string
+	APISecret     string
+	APIKey        string
+	LogFile       string
+	ProductCode   string
+	TradeDuration time.Duration
+	Durations     map[string]time.Duration
+	DbName        string
+	SQLDriver     string
+	Port          int
 }
 
 //Config APIInfo
@@ -25,10 +31,21 @@ func init() {
 		os.Exit(1)
 	}
 
+	durations := map[string]time.Duration{
+		"1s": time.Second,
+		"1m": time.Minute,
+		"1h": time.Hour,
+	}
+
 	Config = APIInfo{
-		APIKey:      cfg.Section("bitflyer").Key("api_key").String(),
-		APISecret:   cfg.Section("bitflyer").Key("api_secret").String(),
-		LogFile:     cfg.Section("fintech").Key("log_file").String(),
-		ProductCode: cfg.Section("fintech").Key("product_code").String(),
+		APIKey:        cfg.Section("bitflyer").Key("api_key").String(),
+		APISecret:     cfg.Section("bitflyer").Key("api_secret").String(),
+		LogFile:       cfg.Section("fintech").Key("log_file").String(),
+		ProductCode:   cfg.Section("fintech").Key("product_code").String(),
+		Durations:     durations,
+		TradeDuration: durations[cfg.Section("gotrading").Key("trade_duration").String()],
+		DbName:        cfg.Section("db").Key("name").String(),
+		SQLDriver:     cfg.Section("db").Key("driver").String(),
+		Port:          cfg.Section("web").Key("port").MustInt(),
 	}
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
 )
