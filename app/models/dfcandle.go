731d262136cf42f59e9a37376ba97009c1619f5b
omoterikuto <omoterikuto@gmail.com>
2021-08-28 15:41:58 +0900

    trade

--- app/models/dfcandle.go
+++ app/models/dfcandle.go
@@ -1,11 +1,46 @@
 package models
 
-import "time"
+import (
+	"btc_trade/tradingalgo"
+	"time"
+
+	"github.com/markcheno/go-talib"
+)
 
 type DataFrameCandle struct {
-	ProductCode string        `json:"product_code"`
-	Duration    time.Duration `json:"duration"`
-	Candles     []Candle      `json:"candles"`
+	ProductCode   string         `json:"product_code"`
+	Duration      time.Duration  `json:"duration"`
+	Candles       []Candle       `json:"candles"`
+	Smas          []Sma          `json:"smas,omitempty"`
+	Emas          []Ema          `json:"emas,omitempty"`
+	BBands        *BBands        `json:"bbands,omitempty"`
+	IchimokuCloud *IchimokuCloud `json:"ichimoku,omitempty"`
+}
+
+type Sma struct {
+	Period int       `json:"period,omitempty"`
+	Values []float64 `json:"values,omitempty"`
+}
+
+type Ema struct {
+	Period int       `json:"period,omitempty"`
+	Values []float64 `json:"values,omitempty"`
+}
+
+type BBands struct {
+	N    int       `json:"n,omitempty"`
+	K    float64   `json:"k,omitempty"`
+	Up   []float64 `json:"up,omitempty"`
+	Mid  []float64 `json:"mid,omitempty"`
+	Down []float64 `json:"down,omitempty"`
+}
+
+type IchimokuCloud struct {
+	Tenkan  []float64 `json:"tenkan,omitempty"`
+	Kijun   []float64 `json:"kijun,omitempty"`
+	SenkouA []float64 `json:"senkoua,omitempty"`
+	SenkouB []float64 `json:"senkoub,omitempty"`
+	Chikou  []float64 `json:"chikou,omitempty"`
 }
 
 func (df *DataFrameCandle) Times() []time.Time {
@@ -54,4 +89,57 @@ func (df *DataFrameCandle) Volume() []float64 {
 		s[i] = candle.Volume
 	}
 	return s
+}
+
+func (df *DataFrameCandle) AddSma(period int) bool {
+	if len(df.Candles) > period {
+		df.Smas = append(df.Smas, Sma{
+			Period: period,
+			Values: talib.Sma(df.Closes(), period),
+		})
+		return true
+	}
+	return false
+}
+
+func (df *DataFrameCandle) AddEma(period int) bool {
+	if len(df.Candles) > period {
+		df.Emas = append(df.Emas, Ema{
+			Period: period,
+			Values: talib.Ema(df.Closes(), period),
+		})
+		return true
+	}
+	return false
+}
+
+func (df *DataFrameCandle) AddBBands(n int, k float64) bool {
+	if n <= len(df.Closes()) {
+		up, mid, down := talib.BBands(df.Closes(), n, k, k, 0)
+		df.BBands = &BBands{
+			N:    n,
+			K:    k,
+			Up:   up,
+			Mid:  mid,
+			Down: down,
+		}
+		return true
+	}
+	return false
+}
+
+func (df *DataFrameCandle) AddIchimoku() bool {
+	tenkanN := 9
+	if len(df.Closes()) >= tenkanN {
+		tenkan, kijun, senkouA, senkouB, chikou := tradingalgo.IchimokuCloud(df.Closes())
+		df.IchimokuCloud = &IchimokuCloud{
+			Tenkan:  tenkan,
+			Kijun:   kijun,
+			SenkouA: senkouA,
+			SenkouB: senkouB,
+			Chikou:  chikou,
+		}
+		return true
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -4,12 +4,10 @@ import (
 	"btc_trade/app/controllers"
 	"btc_trade/config"
 	"btc_trade/utils"
-	"fmt"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	controllers.StreamIngestionData()
-	fmt.Printf("___________")
 	controllers.StartWebServer()
 }
