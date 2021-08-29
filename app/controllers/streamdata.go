49914c097d6d4a7446cfc9c120fd8c47637cd2d0
omoterikuto <omoterikuto@gmail.com>
2021-08-29 17:47:30 +0900

    ai実装

--- app/controllers/streamdata.go
+++ app/controllers/streamdata.go
@@ -8,6 +8,9 @@ import (
 )
 
 func StreamIngestionData() {
+	c := config.Config
+	ai := NewAI(c.ProductCode, c.TradeDuration, c.DataLimit, c.UsePercent, c.StopLimitPercent, c.BackTest)
+
 	var tickerChannl = make(chan bitflyer.Ticker)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannl)
@@ -16,8 +19,8 @@ func StreamIngestionData() {
 			log.Printf("action=StreamIngestionData, %v", ticker)
 			for _, duration := range config.Config.Durations {
 				isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
-				if isCreated == true && duration == config.Config.TradeDuration {
-					// TODO
+				if isCreated && duration == config.Config.TradeDuration {
+					ai.Trade()
 				}
 			}
 		}
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/markcheno/go-talib v0.0.0-20190307022042-cd53a9264d70 // indirect
 	github.com/mattn/go-sqlite3 v1.14.8 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
 )
